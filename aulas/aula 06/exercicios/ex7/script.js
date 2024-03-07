97b349d763d22a1e630dd1a486e315268c40bdca
dudacatalao <dmelocatalao@gmail.com>
2024-03-07 16:21:01 -0300

    updates

--- aulas/aula 06/exercicios/ex7/script.js
+++ aulas/aula 06/exercicios/ex7/script.js
@@ -1,8 +1,11 @@
 function nomes() {
-    let names = ['eduarda', 'ingrid', 'emanuelle', 'maria luiza', 'juliana', 'isabelli', 'giovanna', 'pamella', 'adriana', 'mirair'];
+    const names = ['eduarda', 'ingrid', 'emanuelle', 'maria luiza', 'juliana', 'isabelli', 'giovanna', 'pamella', 'adriana', 'mirair'];
     let stop = false;
+    let tentativas = 0
 
-    while (!stop) {
+
+    while (!stop || tentativas < 5) {
+        tentativas += 1
         let nome = prompt('Insira o nome que deseja pesquisar: ').toLowerCase();
         let found = false;
 
@@ -10,7 +13,6 @@ function nomes() {
             if (nome === names[i]) {
                 alert(`O nome está na nossa lista na posição ${i}`);
                 found = true;
-                break;
             }
         }
 
@@ -18,12 +20,12 @@ function nomes() {
             alert('Nome não encontrado');
         }
 
-        stop = confirm('Deseja continuar pesquisando?');
-        if (stop == true){
-            nomes()
-        }else{
-            break
-        }
+        // stop = confirm('Deseja continuar pesquisando?');
+        // if (stop == true){
+        //     nomes()
+        // }else{
+        //     break
+        // }
     }
 }
 
