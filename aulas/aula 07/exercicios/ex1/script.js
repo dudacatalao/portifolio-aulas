9b40e0d8686a981e356c46ae90f1d8740102910e
Maria Eduarda de Melo Catal� <51550887807@>
2024-03-21 11:27:50 -0300

    updates

--- aulas/aula 07/exercicios/ex1/script.js
+++ aulas/aula 07/exercicios/ex1/script.js
@@ -22,31 +22,67 @@ class Banco {
             alert(`Saldo atual: R$ ${this._saldo}`);
         }
     }
+
+    depositar() {
+        let valor = parseFloat(prompt('Insira o valor que deseja depositar: '))
+
+        if (isNaN(valor) || valor <= 0) {
+            alert('Valor inválido')
+            this.depositar()
+        } else {
+            this._saldo += valor
+            alert(`Saldo atual: R$ ${this._saldo}`)
+        }
+
+    }
 }
 
-class contaCorrente extends Banco{
-    constructor(saldo, numero_cheques, numero_cartaoCredito){
+class contaCorrente extends Banco {
+    constructor(saldo, numero_cheques, numero_cartaoCredito) {
         super(saldo);
         this.numero_cheques = numero_cheques;
         this.numero_cartaoCredito = numero_cartaoCredito;
     }
 
     emitirCheque() {
+        alert('Conta Corrente')
         let beneficiario = prompt('Insira o nome do beneficiário do cheque: ');
         let valorCheque = parseFloat(prompt('Insira o valor do cheque: '));
-        
+
         alert(`Conferindo informações\nBeneficiário: ${beneficiario}\nValor do cheque: R$ ${valorCheque}`);
     }
 }
 
-class contaPoupanca extends Banco{
-    constructor(saldo, limite, limiteTransacoes){
+class contaPoupanca extends Banco {
+    constructor(saldo, taxaJurosAnual) {
         super(saldo);
-        this._limite = limite;
-        this._limiteTransacoes = limiteTransacoes;
+        this._taxaJurosAnual = taxaJurosAnual;
+    }
+
+    renderJuros() {
+        let taxaJurosMensal = this._taxaJurosAnual / 12;
+        let juros = this._saldo * (taxaJurosMensal / 100);
+
+        this._saldo += juros;
+        alert(`Juros adicionados: R$ ${juros.toFixed(2)}\nSaldo atual: R$ ${this._saldo.toFixed(2)}`);
     }
 }
 
-let minhaContaCorrente = new contaCorrente(1000);
-minhaContaCorrente.sacar();
-minhaContaCorrente.emitirCheque(); 
+alert('Bem Vindo(a) ao Sistema Bancário')
+let option = Number(prompt('Deseja ver sua conta\n[1] Corrente\n[2] Poupança'))
+
+if (option == 1){
+    let saldo = Number(prompt('Insira o valor do seu saldo bancario inicial:'))
+    let minhaContaCorrente = new contaCorrente(saldo);
+     minhaContaCorrente.sacar();
+     minhaContaCorrente.depositar()
+     minhaContaCorrente.emitirCheque()
+}else if(option == 2){
+    let saldo = Number(prompt('Insira o valor do seu saldo bancario inicial:'))
+    let minhaContaPoupança = new contaPoupanca(saldo)
+    minhaContaPoupança.sacar()
+    minhaContaPoupança.renderJuros()
+}
+
+ 
+
