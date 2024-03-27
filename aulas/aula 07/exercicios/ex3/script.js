0e839eedc33f072a31c8c5acee31bc604c583ed8
Maria Eduarda de Melo Catal� <51550887807@>
2024-03-27 11:26:05 -0300

    updates

--- aulas/aula 07/exercicios/ex3/script.js
+++ aulas/aula 07/exercicios/ex3/script.js
@@ -9,6 +9,7 @@ class User {
 class Authentication {
     constructor() {
         this.users = [];
+        this.logado = false;
     }
 
     register() {
@@ -32,44 +33,60 @@ class Authentication {
             this.register();
         }
 
-        console.log(this.users)
+        console.log(this.users);
     }
 
     login() {
-        let email = prompt('Insira seu email:')
-        let senha = prompt('Insira sua senha:')
+        let email = prompt('Insira seu email:');
+        let senha = prompt('Insira sua senha:');
 
-        if(email in this.users.email ||senha in this.users.senha ){
-            alert('Logado com sucesso!')
-        }else{
-            alert('Você ainda não possui conta')
-            let option = prompt('Deseja criar uma?')
+        let userFound = this.users.find(user => user.email === email && user.password === senha);
 
-            if (option == true){
-                alert('Redirecionando para o sistema de cadastro')
-                this.register()
-            }else{
-                alert('Obtigada!')
+        if(userFound) {
+            alert('Logado com sucesso!');
+            this.logado = true;
+        } else {
+            alert('Você ainda não possui conta.');
+            let option = prompt('Deseja criar uma conta? (sim/não)').toLowerCase();
+
+            if (option === 'sim') {
+                alert('Redirecionando para o sistema de cadastro.');
+                this.register();
+            } else {
+                alert('Obrigado!');
             }
         }
     }
 
-    logout() {}
+    logout() {
+        if (this.logado === true){
+            this.logado = false;
+            alert('Logout realizado com sucesso');
+        } else {
+            alert('Não tem nenhum usuário logado');
+        }
+    }
 
     menu(){
         alert('Olá, seja bem-vindo ao nosso sistema');
-        let optionUser = Number(prompt('O que deseja realizar\n[1] Realizar Cadastro\n[2] Login\n[3] Logout'))
+        let optionUser = Number(prompt('O que deseja realizar\n[1] Realizar Cadastro\n[2] Login\n[3] Logout'));
 
         switch (optionUser){
             case 1:
-                alert('Redirecionando para o sistema de cadastro..')
-                this.register()
+                alert('Redirecionando para o sistema de cadastro..');
+                this.register();
+                break;
             case 2:
-                alert('Redirecionando para a área de Login..')
-                this.login()
+                alert('Redirecionando para a área de Login..');
+                this.login();
+                break;
             case 3:
-                alert('Redirecionando para a área de Logout')
-                this.logout()
+                alert('Redirecionando para a área de Logout');
+                this.logout();
+                break;
+            default:
+                alert('Opção inválida');
+                break;
         }
     }
 }
